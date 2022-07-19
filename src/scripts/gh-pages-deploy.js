fc3079f00572f0cb8549cf27d5c77bc1ef265d2b
Mirko Rainer <mirkorainer@outlook.com>
2022-07-19 05:57:13 -0400

    Add deploy script and tools.

---
+++ src/scripts/gh-pages-deploy.js
@@ -0,0 +1,33 @@
+/* eslint-disable */
+const execa = require("execa");
+const fs = require("fs");
+(async () => {
+  try {
+    await execa("git", ["checkout", "--orphan", "gh-pages"]);
+    // eslint-disable-next-line no-console
+    console.log("Building started...");
+    await execa("npm", ["run", "build"]);
+    // Understand if it's dist or build folder
+    const folderName = fs.existsSync("dist") ? "dist" : "build";
+    const website = 'bitcoinservices.info';
+    fs.writeFile(`${folderName}/CNAME`, website, err => {
+      if (err) {
+        console.error(err);
+      }
+      console.log(`CNAME file created: ${website}`);
+    });
+    await execa("git", ["--work-tree", folderName, "add", "--all"]);
+    await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
+    await execa("echo", ["bitcoinservices.info", ">", "CNAME"]);
+    console.log("Pushing to gh-pages...");
+    await execa("git", ["push", "origin", "HEAD:gh-pages", "--force"]);
+    await execa("rm", ["-r", folderName]);
+    await execa("git", ["checkout", "-f", "main"]);
+    await execa("git", ["branch", "-D", "gh-pages"]);
+    console.log("Successfully deployed, check your settings");
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.log(e.message);
+    process.exit(1);
+  }
+})();
