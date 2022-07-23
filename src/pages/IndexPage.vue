36377d7cbaf05d4b8a0987f31ffdac85284dba8b
Mirko Rainer <mirkorainer@outlook.com>
2022-07-23 19:52:45 -0400

    Filtering!

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -6,14 +6,21 @@
       elevated
       class="bg-primary text-white"
     >
-      <q-scroll-area class="fit">
+      <q-scroll-area :class="q$.dark.isActive ? 'fit bg-black' : 'fit'">
         <div class="q-pa-sm">
           <div class="q-gutter-sm">
+            <div class="q-pa-sm text-h5">Locales</div>
             <div v-for="locale in locales" :key="locale">
-              <q-checkbox dense v-model="filterObject.locales" :dark="q$.dark.isActive" :val="locale" :label="locale.toLocaleUpperCase()" />
+              <q-checkbox dense v-model="filterLocales" :dark="q$.dark.isActive" :color="q$.dark.isActive ? 'secondary' : ''" :val="locale" :label="locale.toLocaleUpperCase()" />
             </div>
-            <div class="q-px-sm">
-              The model data: <strong>{{ filterObject.locales }}</strong>
+          </div>
+        </div>
+        <q-separator />
+        <div class="q-pa-sm">
+          <div class="q-gutter-sm">
+            <div class="q-pa-sm text-h5">Categories</div>
+            <div v-for="category in categories" :key="category">
+              <q-checkbox dense v-model="filterCategories" :dark="q$.dark.isActive" :val="category" :label="category.toLocaleUpperCase()" />
             </div>
           </div>
         </div>
@@ -22,7 +29,7 @@
   <q-page class="row items-center justify-evenly flex">
     <div class="flex-start">
       <q-btn :class="q$.dark.isActive ? 'q-pa-sm text-black' : 'q-pa-sm'" :color="q$.dark.isActive ? 'secondary' : 'primary'" @click="drawerLeft = !drawerLeft" :icon="drawerLeft ? 'eva-arrow-left-outline' : 'eva-arrow-right-outline'" label="Filter"/>
-      </div>
+    </div>
     <div class="q-pa-sm justify-left">
       <q-btn :class="q$.dark.isActive ? 'q-pa-sm text-black' : 'q-pa-sm'" :color="q$.dark.isActive ? 'secondary' : 'primary'" @click="showHelpDialog = !showHelpDialog" icon="eva-github-outline" label="Are we missing something?"/>
     </div>
@@ -48,55 +55,48 @@
         </q-card-actions>
       </q-card>
     </q-dialog>
-    <q-table
-      :fullscreen="isFullscreen"
-      :columns="columns"
-      :rows="data"
-      style="width: 90vw"
-      :pagination="pagination"
-      grid
-      :filter="filterObject"
-      :filter-method="filterFunction"
-    >
-      <template v-slot:item="props">
-        <div class="q-pa-xs col-xs-12 col-sm-6 col-md-4">
-          <q-card>
-            <q-card-section :class="q$.dark.isActive ? 'text-center text-h3 bg-secondary text-black' : 'text-center text-h3 bg-primary text-white'">
-              <strong>{{ props.row.name }}</strong>
-            </q-card-section>
-              <q-card-section class="flex flex-center q-pa-sm">
-                <div :class="q$.dark.isActive ? 'text-center text-white' :'text-center'"><a  :class="q$.dark.isActive ? 'text-white' :''" :href="props.row.url">{{ props.row.url }}</a></div>
-              </q-card-section>
-            <q-separator />
-            <q-card-section class="flex flex-center">
-              <div class="text-center">{{ props.row.description }}</div>
-            </q-card-section>
-            <q-separator />
-            <q-card-section class="q-pa-sm">
-              <div class="flex q-pa-sm">
-                <div class="flex content-center">Locales:</div>
-              </div>
-              <div class="row">
-                <div class="text-center q-pa-sm flex" v-for="locale in props.row.locales" :key="locale">
-                  <q-chip :class="q$.dark.isActive ? 'bg-secondary text-black' : 'bg-primary text-white'">{{ Locale[locale] }}</q-chip>
-                </div>
-              </div>
-            </q-card-section>
-            <q-card-section class="q-pa-sm">
-              <div class="flex q-pa-sm">
-                <div class="flex content-center">Category:</div>
-              </div>
-              <div class="row">
-                <div class="text-center q-pa-sm flex" v-for="category in props.row.categories" :key="category">
-                  <q-chip :class="q$.dark.isActive ? 'bg-secondary text-black' : 'bg-primary text-white'">{{ ServiceCategory[category] }}</q-chip>
-                </div>
-              </div>
-            </q-card-section>
+    <div class="row items-center justify-center flex">
 
-          </q-card>
+    <div v-for="row in rows" :key="row.name">
+    <div class="q-pa-xs col-xs-12 col-sm-6 col-md-4" style="width: 350px">
+      <q-card>
+        <q-card-section :class="q$.dark.isActive ? 'text-center text-h3 bg-secondary text-black' : 'text-center text-h3 bg-primary text-white'">
+          <strong>{{ row.name }}</strong>
+        </q-card-section>
+          <q-card-section class="flex flex-center q-pa-sm">
+            <div :class="q$.dark.isActive ? 'text-center text-white' :'text-center'"><a  :class="q$.dark.isActive ? 'text-white' :''" :href="row.url">{{ row.url }}</a></div>
+          </q-card-section>
+        <q-separator />
+        <q-card-section class="flex flex-center">
+          <div class="text-center">{{ row.description }}</div>
+        </q-card-section>
+        <q-separator />
+        <q-card-section class="q-pa-sm">
+          <div class="flex q-pa-sm">
+            <div class="flex content-center">Locales:</div>
+          </div>
+          <div class="row">
+            <div class="text-center q-pa-sm flex" v-for="locale in row.locales" :key="locale">
+              <q-chip :class="q$.dark.isActive ? 'bg-secondary text-black' : 'bg-primary text-white'">{{ Locale[locale] }}</q-chip>
+            </div>
+          </div>
+        </q-card-section>
+        <q-card-section class="q-pa-sm">
+          <div class="flex q-pa-sm">
+            <div class="flex content-center">Category:</div>
+          </div>
+          <div class="row">
+            <div class="text-center q-pa-sm flex" v-for="category in row.categories" :key="category">
+              <q-chip :class="q$.dark.isActive ? 'bg-secondary text-black' : 'bg-primary text-white'">{{ ServiceCategory[category] }}</q-chip>
+            </div>
+          </div>
+        </q-card-section>
+
+      </q-card>
+    </div>
         </div>
-      </template>
-      </q-table>
+    </div>
+
   </q-page>
 </template>
 
@@ -104,7 +104,7 @@
 import { useQuasar } from 'quasar';
 import { Locale, Service, ServiceCategory } from 'src/components/models';
 import { data } from 'src/data/data';
-import { defineComponent, ref, watch } from 'vue';
+import { defineComponent, Ref, ref, watch } from 'vue';
 
 const columns = [
   {
@@ -157,12 +157,6 @@ const pagination = {
     rowsPerPage: 25
 }
 
-interface FilterObject {
-  nameString: string,
-  locales: string[],
-  categories: string[]
-}
-
 export default defineComponent({
   name: 'IndexPage',
   setup() {
@@ -172,41 +166,48 @@ export default defineComponent({
     const drawerLeft = ref(false);
     const locales = Object.keys(Locale).filter((v) => isNaN(Number(v)));
     const categories = Object.keys(ServiceCategory).filter((v) => isNaN(Number(v)));
-    const filterObject = ref({
-      nameString: '',
-      locales,
-      categories
-    } as FilterObject)
+    const filterLocales = ref(locales)
+    const filterCategories = ref(categories);
+    const rows = ref(data);
 
-    watch(() => filterObject.value.locales, (newVal) => {
-      console.log(JSON.stringify(newVal));
+    watch(filterLocales, () => {
+      rows.value = data;
+      rows.value = filterData(rows, filterLocales, filterCategories);
     })
 
+    watch(filterCategories, () => {
+      rows.value = data;
+      rows.value = filterData(rows, filterLocales, filterCategories);
+    })
 
-
-    function filterFunction(rows: Service[], terms: FilterObject): Array<Service> {
-      return rows.filter(x => {
-          const categoriesInFilter = x.categories.filter(y => terms.categories.indexOf(ServiceCategory[y]) !== -1);
-        const localeInFilter = x.locales.filter(y => terms.locales.indexOf(Locale[y]) !== -1);
-
-        return localeInFilter.length > 0 || categoriesInFilter.length > 0;
-      })
+    function filterData(array: Ref<Service[]>, localesFilter: Ref<string[]>, categoriesFilter: Ref<string[]>): Service[] {
+      return array.value.filter(x => {
+        const categoriesInFilter=x.categories.filter(y => categoriesFilter.value.indexOf(ServiceCategory[y])!==-1);
+        const localeInFilter=x.locales.filter(y => localesFilter.value.indexOf(Locale[y])!==-1);
+        return localeInFilter.length!=0 && categoriesInFilter.length!=0;
+      });
     }
+
+    function filter() {
+      rows.value = filterData(rows, filterLocales, filterCategories);
+    }
+
 
     return {
       isFullscreen,
       columns,
-      data,
+      rows,
       pagination,
       Locale,
       ServiceCategory,
       q$,
       showHelpDialog,
       drawerLeft,
-      filterObject,
-      filterFunction,
+      filter,
       categories,
-      locales
+      locales,
+      filterLocales,
+      filterCategories
     };
   }
 });
